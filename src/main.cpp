4759da02eafd9c438f79f748bb342d8a724f91d5
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2021-08-16 15:29:50 +0200

    better example of use

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,26 @@
 #include "pngUtilities.h"
 
 int main(){
-    std::string text = steg::getContents("textFiles/cppWiki.txt");
-    std::string filename = "pictures/nature.png";
-    steg::lsb::encode(filename, text);
-    std::string decodedText = steg::lsb::decode(steg::encodedFilename(filename));
-    steg::saveFile("textFiles/decoded.txt", decodedText);
+    // How to encode and decode text
+    {
+        std::cout << "Encoding text\n";
+        std::string text = steg::getContents("textFiles/cppWiki.txt");
+        std::string filename = "pictures/nature.png";
+        steg::lsb::encode(filename, text);
+        std::cout << "Decoding text\n";
+        std::string decodedText = steg::lsb::decode(steg::encodedFilename(filename));
+        steg::saveFile("textFiles/decoded.txt", decodedText);
+    }
+    // How to encode and decode pictures
+    {
+        std::cout << "Encoding picture\n";
+        std::string data = steg::getContents("pictures/mountain.png");
+        std::string filename = "pictures/nature.png";
+        steg::lsb::encode(filename, data);
+        std::cout << "Decoding picture\n";
+        std::string decodedData = steg::lsb::decode(steg::encodedFilename(filename));
+        steg::saveFile("pictures/decoded.png", decodedData);
+    }
+    std::cout << "Finished!\n";
     return 0; 
 }
